ec5527c14c383506e94352fd454b7829d1f8d4e1
hugolesati <hugolesati@hotmail.com>
2021-07-28 20:33:34 -0300

    Desafio clase 2

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/* var dinero = prompt("Usted tiene dinero para un plazo fijo?");
+
+if (dinero == "Si") {
+    alert("Gracias, usted tiene un plazo fijo");
+} else if (dinero == "No") {
+    alert("Usted no dispone dinero para un plazo fijo");
+} else {
+    alert("Debes decirnos si tienes o no dinero");
+} */
+
+/* let nombre = prompt("Ingresa el nombre del personaje"); */
+
+/* if (nombre == "Harry" || nombre == "harry") {
+  alert("Ingresaste el personaje Harry");
+}
+if (nombre == "Hermione" || nombre == "hermione") {
+  alert("Ingresaste a Hermione");
+}
+if (nombre == "Ron" || nombre == "ron") {
+  alert("Ingresaste a Ron");
+}
+if (nombre == "Voldemort" || nombre == "voldemort") {
+  alert("No debiste nombrarlo");
+} */
+
+/* if ((nombre != "") && (nombre == "Harry" || nombre == "harry" || nombre == "HARRY" || nombre == "Ron" || nombre == "ron" || nombre == "RON" || nombre == "Hermione" || nombre == "hermione" || nombre == "HERMIONE")) {
+  alert("Ingresaste el personaje " + nombre);
+} else {
+  alert("Debes ingresar un personaje valido")
+} */
+
+let  ironMan = prompt("¿Quien usa el traje de Iron Man?")
+
+if ((ironMan != "") && (ironMan == "Tony Stark" || ironMan == "tony stark" || ironMan == "TONY STARK")){
+  alert("El dueño del traje de Iron Man es " + ironMan);
+} else {
+  alert("DEBES INGRESAR AL DUEÑO DEL TRAJE");
+}
